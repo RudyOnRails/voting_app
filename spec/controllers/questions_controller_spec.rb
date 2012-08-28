b99b81ac12dfaf906c01eb5ef6e3cf2d342b24ad
Kevin Musiorski <musiorski@gmail.com>
2012-08-28 11:52:07 -0500

    finished questionscontroller spec and also had to define invalid_question factory

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,19 +1,41 @@
 require 'spec_helper'
 
+# You should test for things such as:
+
+# was the web request successful?
+# was the user redirected to the right page?
+# was the user successfully authenticated?
+# was the correct object stored in the response template?
+# was the appropriate message displayed to the user in the view?
+
+# test for json vs html (Ryan suggested, but I'm not sure where to start, will goog)
+
 describe QuestionsController do
-  describe "#index" do
-    it "assigns @questions" do
+  describe "GET #index" do
+    it "populates an array of questions" do
       question = FactoryGirl.create(:question)
       get :index
       expect(assigns(:questions)).to eq([question])
     end
-    it "renders index template" do
+    it "renders the :index view" do
       get :index
       response.should render_template("index")
-      response.code.should eq("200")
     end
   end
-  describe "#new" do
+
+  describe "GET #show" do
+    it "assigns the requested question to @question" do
+      question = FactoryGirl.create(:question)
+      get :show, :id => question
+      assigns(:question).should eq(question)
+    end
+    it "renders the #show view" do
+      get :show, :id => FactoryGirl.create(:question)
+      response.should render_template :show
+    end
+  end
+
+  describe "GET #new" do
     it "renders new template" do
       get :new
       assert_response :success
@@ -22,26 +44,121 @@ describe QuestionsController do
       Question.new.should be_an_instance_of(Question)
     end
   end
-  describe "#create" do
-    it "takes in params and make new variable"
-    it "saves new variable in database"
-    it "redirects to index page"
+
+  describe "POST #create" do
+
+    context "with valid attributes" do
+
+      it "creates a new question" do
+        expect{
+          post :create, question: FactoryGirl.attributes_for(:question)
+        }.to change(Question, :count).by(1)
+      end
+
+      it "redirects to the new question" do
+        post :create, question: FactoryGirl.attributes_for(:question)
+        response.should redirect_to Question.last
+      end
+
+    end
+
+    context "with invalid attributes" do
+
+      it "does not save the new question" do
+        expect{
+          post :create, question: FactoryGirl.attributes_for(:question_invalid)
+        }.to_not change(Question, :count)
+      end
+
+      it "re-renders the new method" do
+        post :create, question: FactoryGirl.attributes_for(:question_invalid)
+        response.should render_template :new
+      end
+
+    end
   end
-  describe "#show" do
-    it "passes the :id to show"
-    it "finds that object in the db using the :id"
-    it "renders the show view"
+
+  describe "GET #edit" do
+
+    before :each do
+      @question = FactoryGirl.create(:question)
+    end
+
+    it "finds the object and set it equal to a variable" do
+      get :edit, :id => @question, :question => FactoryGirl.attributes_for(:question)
+      assigns(:question).should eq(@question)
+    end
+
+    it "renders the edit view" do
+      get :edit, :id => FactoryGirl.create(:question)
+      response.should render_template :edit
+    end
   end
-  describe "#edit" do
-    it "finds the object and set it equal to a variable"
-    it "renders the edit view"
+
+  describe "PUT #update" do
+
+    before :each do
+      @question = FactoryGirl.create(:question, title: "What the fuck?")
+    end
+
+    context "valid params" do
+
+      it "locate the requested @question" do
+        put :update, :id => @question, :question => FactoryGirl.attributes_for(:question)
+        assigns(:question).should eq(@question)
+      end
+
+      it "changes @question's attributes" do
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question, title: "What the FUCK?")
+        @question.reload
+        @question.title.should eq("What the FUCK?")
+      end
+
+      it "redirects to the upudated question" do
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question)
+        response.should redirect_to @question
+      end
+
+    end
+
+    context "invalid params" do
+
+      it "locate the requested @question" do
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question_invalid)
+        assigns(:question).should eq(@question)
+      end
+    
+      it "does not change @question's attributes" do
+          put :update, id: @question, question: FactoryGirl.attributes_for(:question, title: "")
+          @question.reload
+          @question.title.should_not eq("")
+          @question.title.should eq("What the fuck?")
+      end
+    
+      it "re-renders the edit method" do
+        put :update, id: @question, question: FactoryGirl.attributes_for(:question_invalid)
+        response.should render_template :edit
+      end
+    end
+
   end
-  describe "#update" do
-    describe "valid params"
-    describe "invalid params"
+
+  describe "DELETE #destroy" do
+
+    before :each do
+      @question = FactoryGirl.create(:question)
+    end
+
+    it "takes in the :id, and destroy the question" do
+      expect{
+        delete :destroy, id: @question
+        }.to change(Question, :count).by(-1)
+    end
+
+    it "redirects to questions#index" do
+      delete :destroy, id: @question
+      response.should redirect_to questions_url
+    end
   end
-  describe "#destroy" do
-    it "takes in the :id, and destroy the question"
-    it "redirects to questions index"
-  end
+
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,13 @@
 require 'faker'
 
 FactoryGirl.define do
+
   factory :question do |f|
     f.title { Faker::Lorem.words }
   end
+
+  factory :question_invalid, parent: :question do |f|
+    f.title nil
+  end
+
 end
